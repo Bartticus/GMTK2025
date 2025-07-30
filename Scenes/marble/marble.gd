642d9d9d74dfea93f0884505a6bf9ad81b4473c9
Thaddeus Psarris <102464319+steamedowl@users.noreply.github.com>
2025-07-30 15:48:49 -0700

    Separated ball physics from visuals
    -Camera is now parented to anchor and operations previously done to camera are now done to anchor, so that true camera basis can be set as normal and then camera can do smoothed lerping and slight movements on top of that.
    -Moved ball mesh into Visuals node that will handle everything visual, and started work on juice scaling tests
    -Created test environment for rolling ball around into obstacles

--- Scenes/marble/marble.gd
+++ Scenes/marble/marble.gd
@@ -2,25 +2,28 @@ extends RigidBody3D
 
 @export var move_speed: float = 400
 @export var max_velocity: float = 10
-@onready var camera: Camera3D = $Camera3D
+@onready var visuals : Node3D = $Visuals
+@onready var camera_anchor: Node3D = $CameraAnchor
+@onready var camera: Camera3D = $CameraAnchor/Camera3D
 var camera_initial_pos: Vector3
 
 func _ready() -> void:
-	camera.top_level = true
-	camera_initial_pos = camera.position
+	camera_anchor.top_level = true
+	camera_initial_pos = camera_anchor.position
 	
 
 func _physics_process(delta: float) -> void:
 	movement_handler(delta)
+	visuals.visuals_handler(delta)
 	
-	camera.global_position = global_position + camera_initial_pos
+	camera_anchor.global_position = global_position + camera_initial_pos
 	
 
 func movement_handler(delta: float) -> void:
 	var f_input = Input.get_action_raw_strength("backward") - Input.get_action_raw_strength("forward")
 	var h_input = Input.get_action_raw_strength("right") - Input.get_action_raw_strength("left")
 	
-	var camera_transform = camera.get_camera_transform()
+	var camera_transform = camera_anchor.global_transform#get_camera_transform()
 	
 	var f_force = f_input * move_speed * delta * camera_transform.basis.z.normalized()
 	var h_force = h_input * move_speed * delta * camera_transform.basis.x.normalized()
