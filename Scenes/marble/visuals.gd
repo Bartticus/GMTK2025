eadc1509012c9b6c22965a9d341a4681f859db16
Thaddeus Psarris <102464319+steamedowl@users.noreply.github.com>
2025-07-30 17:59:11 -0700

    Better collision detection and FX

--- Scenes/marble/visuals.gd
+++ Scenes/marble/visuals.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export_range(0.0, 3.0, 0.1) var scale_distortion_scale : float = 1.0
+@export_range(0.0, 20.0, 0.3) var impulse_minimum_impact : float = 7.0
 @export var scale_distortion_start_velocity : float = 3.0
 @export var squash_distortion_max_velocity : float = 20.0#we won't squash any more even if we're going faster than this
 
@@ -9,22 +10,30 @@ extends Node3D
 
 @export var marble : RigidBody3D
 @onready var marble_mesh : MeshInstance3D = $MarbleMesh
+@onready var impact_fx : Node3D = $ImpactFX
 @onready var smoothed_velocity : Vector3 = Vector3.ZERO
 
 
 @export var squash_length : float = 0.3
 @onready var squash_timer : float = 0.0
 @onready var squash_intensity : float = 0.0
-@onready var velocity_last_frame : Vector3 = Vector3.ZERO
 
 func _ready() -> void:
 	top_level = true
+	impact_fx.top_level = true
+	impact_fx.visible = false
 
 
-
-func impact(intensity : float = 0.0)->void:#intensity is 0-1
+func impact(intensity : float = 0.0, contact_position : Vector3 = Vector3.ZERO, normal : Vector3 = Vector3.ZERO)->void:#intensity is 0-1
 	squash_timer = squash_length
-	squash_intensity = lerpf(0.0, 5.0, intensity)
+	squash_intensity = lerpf(0.0, 0.5, intensity)
+	
+	var fx : Node3D = impact_fx.duplicate()
+	add_child(fx)
+	fx.visible = true
+	fx.global_position = contact_position
+	fx.look_at(contact_position+normal)
+	fx.sprite.rotate_z(randf())
 
 func visuals_handler(delta: float) -> void:
 	set_identity()
@@ -39,18 +48,24 @@ func visuals_handler(delta: float) -> void:
 	var speed_factor : float = current_speed / 14.0
 	speed_factor = min(speed_factor, 1.0)
 	
-	var speed_change : float = velocity_last_frame.length() - marble.linear_velocity.length()
-	var impact_scale : float = marble.linear_velocity.angle_to(velocity_last_frame) - 1.0
-	if impact_scale > 0.0:
-		impact_scale /= squash_distortion_max_velocity
+	
+	var state = PhysicsServer3D.body_get_direct_state(marble.get_rid())
+	var contact_count : int = state.get_contact_count()
+	var biggest_impulse : float = 0.0
+	var contact_position : Vector3 = Vector3.ZERO
+	var contact_normal : Vector3 = Vector3.ZERO
+	for i in contact_count:
+		if biggest_impulse < state.get_contact_impulse(i).length():
+			contact_position = state.get_contact_local_position(i)
+			contact_normal = state.get_contact_local_normal(i)
+		biggest_impulse = max(biggest_impulse, state.get_contact_impulse(i).length())
+	
+	
+	biggest_impulse -= impulse_minimum_impact
+	if biggest_impulse > 0.0:
+		var impact_scale = biggest_impulse / squash_distortion_max_velocity
 		impact_scale = min(impact_scale, 1.0)
-		impact(impact_scale)
-	else:
-		impact_scale = speed_change - 5.0
-		if impact_scale > 0.0:
-			impact_scale /= squash_distortion_max_velocity
-			impact_scale = min(impact_scale, 1.0)
-			impact(impact_scale)
+		impact(impact_scale, contact_position, contact_normal)
 	
 	
 	var new_scale : Vector3 = Vector3.ONE
@@ -65,5 +80,3 @@ func visuals_handler(delta: float) -> void:
 		new_scale.x += squash_x_curve.sample_baked(squash_lerp) * squash_intensity
 	
 	scale_object_local(new_scale)
-	
-	velocity_last_frame = marble.linear_velocity
