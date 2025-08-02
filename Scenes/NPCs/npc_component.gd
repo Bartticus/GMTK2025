f3debef8b8b249995b75245ebb1e65f3beb6a958
Thaddeus Psarris <102464319+steamedowl@users.noreply.github.com>
2025-08-02 13:09:13 -0700

    squishier npc

--- Scenes/NPCs/npc_component.gd
+++ Scenes/NPCs/npc_component.gd
@@ -41,37 +41,48 @@ func _process(delta: float) -> void:
 	else:
 		e_prompt.modulate.a = lerpf(e_prompt.modulate.a, 0.0, delta * 10.0)
 	
+	
+	if going_down:
+		height_lerp -= delta
+		height_lerp = max(height_lerp, 0.0)
+		if height_lerp == 0.0:
+			going_down = false
+	else:
+		height_lerp += delta
+		height_lerp = min(height_lerp, 1.0)
+		if height_lerp == 1.0:
+			going_down = true
+	
+	
+	
 	if e_prompt.modulate.a > 0.05:
-		if going_down:
-			height_lerp -= delta
-			height_lerp = max(height_lerp, 0.0)
-			if height_lerp == 0.0:
-				going_down = false
-		else:
-			height_lerp += delta
-			height_lerp = min(height_lerp, 1.0)
-			if height_lerp == 1.0:
-				going_down = true
-		
 		e_prompt.position.y = lerpf(0.845, 0.945, hover_curve.sample_baked(height_lerp))
 	
+	var new_scale : Vector3 = Vector3.ONE
 	if squishing:
-		squash_timer += delta
+		squash_timer -= delta
 		squash_timer = max(squash_timer, 0.0)
-		var squash_lerp : float = lerpf(0.0, 1.0, squash_timer / squash_length)
-		var new_scale = Vector3.ONE
+		var squash_lerp : float = lerpf(1.0, 0.0, squash_timer / squash_length)
+		
 		new_scale.y += Global.player.visuals.squash_z_curve.sample_baked(squash_lerp) * squash_intensity
 		new_scale.x += Global.player.visuals.squash_x_curve.sample_baked(squash_lerp) * squash_intensity
 		new_scale.z += Global.player.visuals.squash_x_curve.sample_baked(squash_lerp) * squash_intensity
 		
-		character_visuals.scale = new_scale
-
+		if squash_timer == 0.0:
+			squishing = false
+	else:
+		var breathing_scale : float = 0.15
+		var breathing_lerp : float = hover_curve.sample_baked(height_lerp) - 1.0
+		#breathing_lerp -= 1.0
+		new_scale.x += breathing_lerp * breathing_scale
+		new_scale.z += breathing_lerp * breathing_scale
+		new_scale.y -= breathing_lerp * breathing_scale
+	character_visuals.scale = new_scale
+	
 
 func next_line():
-	squash_timer = 0.0
+	squash_timer = squash_length
 	squishing = true
-
-
 
 
 
