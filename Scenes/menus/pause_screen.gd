67550566792f4a5f9504fa044cf2747824a5ad9c
MattMakesNoise <mattpaquinbusiness@gmail.com>
2025-08-02 20:32:22 -0400

    UI sfxs

--- Scenes/menus/pause_screen.gd
+++ Scenes/menus/pause_screen.gd
@@ -2,11 +2,15 @@ extends Control
 
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
 
+@onready var sfx : AudioStreamPlayer = $uisfx
+@onready var sfxH : AudioStreamPlayer = $uiHoversfx
+
 func _ready() -> void:
 	hide()
 	anim_player.play("RESET")
 
 func resume() -> void:
+	sfx.play()
 	get_tree().set_deferred("paused", false)
 	hide()
 	anim_player.play_backwards("blur")
@@ -32,4 +36,20 @@ func _on_restart_pressed() -> void:
 	get_tree().reload_current_scene()
 
 func _on_quit_pressed() -> void:
+	sfx.play()
 	get_tree().quit()
+
+
+func _on_resume_mouse_entered() -> void:
+	sfxH.play()
+	pass # Replace with function body.
+
+
+func _on_restart_mouse_entered() -> void:
+	sfxH.play()
+	pass # Replace with function body.
+
+
+func _on_quit_mouse_entered() -> void:
+	sfxH.play()
+	pass # Replace with function body.
